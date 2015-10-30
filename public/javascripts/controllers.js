23fd9c5b32f41c996b5afe96a51b3fc625f25eef
amhayslip <amhayslip@gmail.com>
2015-10-30 10:38:34 +0200

    Stub beers

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -5,4 +5,10 @@ angular.module("beerReview.controllers", [])
 function($scope){
   // $scope.test = 'Hello world!';
   $scope.test = "hello world";
+
+  $scope.beers = [
+  	{ name: "Mayday IPA", rating: 5},
+  	{ name: "Pout Stout", rating: 8},
+  	{ name: "Fail Pale Ale", rating: 4}
+  ]
 }]);
