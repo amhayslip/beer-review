c11fcd80ace1387c95da7660f385f1f6ca955a9d
amhayslip <amhayslip@gmail.com>
2015-10-30 11:11:25 +0200

    add ability to post new beers

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -5,4 +5,14 @@ angular.module("beerReview.controllers", [])
 'beers',
 function($scope, beers){
   $scope.beers = beers.beers;
+
+  $scope.addBeer = function() {
+  	if ($scope.name === '') { return; }
+  	$scope.beers.push({ 
+  		name: $scope.name, 
+  		ratine: $scope.rating
+  	});
+  	$scope.name = '';
+  	$scope.rating = '';
+  }
 }]);
--- public/javascripts/factories.js
+++ public/javascripts/factories.js
@@ -3,10 +3,10 @@ angular.module("beerReview.factories", [])
 .factory('beers', [function(){
   var o = {
   	beers: [
-  	  	{ name: "Mayday IPA", rating: 5},
-  	  	{ name: "Pout Stout", rating: 8},
-  	  	{ name: "Fail Pale Ale", rating: 4}
-  	  ]
+	  	{ name: "Mayday IPA", rating: 5},
+	  	{ name: "Pout Stout", rating: 8},
+	  	{ name: "Fail Pale Ale", rating: 4}
+  	]
   };
   return o;
 }]);
