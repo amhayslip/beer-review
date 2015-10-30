834fe0f68d97373fa653f65aee4ae5b0c075c28b
amhayslip <amhayslip@gmail.com>
2015-10-30 10:33:05 +0200

    hello world it

---
+++ public/javascripts/controllers.js
@@ -0,0 +1,8 @@
+angular.module("beerReview.controllers", [])
+
+.controller('MainController', [
+'$scope',
+function($scope){
+  // $scope.test = 'Hello world!';
+  $scope.test = "hello world";
+}]);
